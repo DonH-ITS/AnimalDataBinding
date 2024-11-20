e69164d9922fd6ccaa8214f4587a3f11f9fba7a1
donny.hurley@atu.ie <donny.hurley@atu.ie@CR436-01-8T785R>
2024-11-20 12:28:58 +0000

    Add project files.

---
+++ MauiApp2/AnimalViewModel.cs
@@ -0,0 +1,75 @@
+﻿
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiApp2
+{
+    public class AnimalViewModel : INotifyPropertyChanged
+    {
+        private string title;
+        private List<Animal> animalList;
+        private HttpClient httpClient;
+
+        private ObservableCollection<Animal> _animals;
+        public ObservableCollection<Animal> Animals { get => _animals; }
+
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                if (isBusy != value)
+                {
+                    isBusy = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(IsNotBusy));
+                }
+            }
+        }
+        public bool IsNotBusy => !IsBusy;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public AnimalViewModel()
+        {
+            httpClient = new HttpClient();
+            animalList = new List<Animal>();
+        }
+
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task<string> GetAnimals()
+        {
+            var response = await httpClient.GetAsync("https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            return "";
+        }
+
+
+    }
+}
---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        private AnimalViewModel animalViewModel;
+    
+        public MainPage()
+        {
+            InitializeComponent();
+            animalViewModel = new AnimalViewModel();
+            BindingContext = animalViewModel;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            infoLabel.Text = await animalViewModel.GetAnimals();
+        }
+    }
+
+}
